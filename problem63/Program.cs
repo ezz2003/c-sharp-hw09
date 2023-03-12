deb86a7defff0eb9730bebd86960f2f3312837b7
ezz2003 <ezz2003@yandex.ru>
2023-03-13 08:33:53 +1000

    solved the problem63

---
+++ problem63/Program.cs
@@ -0,0 +1,17 @@
+﻿// 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа от 1 до N
+
+int InputInt(string message)
+{
+    Console.Write(message + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintSequence(int n, int i = 1)
+{
+    if (n == 0) return;
+    PrintSequence(n - 1);
+    Console.Write(n+" ");
+}
+
+int num = InputInt("Введите число");
+PrintSequence(num);

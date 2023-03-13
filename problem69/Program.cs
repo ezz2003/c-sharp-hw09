fe911471d8df8ed60533fdc9dd17f71d96106b83
ezz2003 <ezz2003@yandex.ru>
2023-03-13 14:14:04 +1000

    solved the problem69

---
+++ problem69/Program.cs
@@ -0,0 +1,20 @@
+﻿// 69.Напишите программу, которая на вход ринимает два числа А и В, и возводит А в целую степень В с помощью рекурсии.
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int CustomPow(int num, int deg)
+{
+  if (deg == 0)
+    return 1;
+  else
+    return num * CustomPow(num, deg - 1);
+}
+
+
+int number = InputInt("Введите число");
+int degree = InputInt("Введите степень числа");
+Console.WriteLine(CustomPow(number, degree));

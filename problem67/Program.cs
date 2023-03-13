7dacbea52d89c800e438fcb98022a5e8ed7bf24c
ezz2003 <ezz2003@yandex.ru>
2023-03-13 13:38:52 +1000

    solved the problem67

---
+++ problem67/Program.cs
@@ -0,0 +1,19 @@
+﻿// 67. Напишите программу, которая будет принимать на вход число, и выдавать сумму его цифр.
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummDigits(int num)
+{
+  if (num == 0)
+    return 0;
+  else
+    return num % 10 + SummDigits(num / 10);
+}
+
+
+int number = InputInt("Введите число");
+Console.WriteLine(SummDigits(number));
